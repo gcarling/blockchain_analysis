d8a7ded533109b453dc66295b6f3074417734c1f
jfedor <jfedor@cs.brown.edu>
2014-04-28 19:14:48 -0400

    to appengine

--- test.py
+++ test.py
@@ -2,8 +2,11 @@ import query
 import format
 
 def main():
-	res = query.explore("13x2FVN4N6ahtbWCthKF3cArxrH9GJMNPg",layers=2)
+	
 
+	#res = query.explore("13x2FVN4N6ahtbWCthKF3cArxrH9GJMNPg",layers=2)
+
+	res = query.explore(max_nodes=10,predicate=query.round_numbers)
 
 	transactions = []
 	for addr in res.itervalues():
@@ -13,8 +16,15 @@ def main():
 		# print "balance: "+str(addr.balance)
 		for tx in addr.tx:
 			transactions.append(tx)
-			
+
 	format.transactions_to_graph(transactions)
+
+def group_entity():
+
+	
+
+	for addr in res.itervalues():
+		print addr.tx
 
 if __name__ == "__main__":
 	main()
