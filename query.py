c9ad310123805ee923544eec8fb95fc89b7c5d3b
Graham Carling <gramcarling@aol.com>
2014-04-28 18:00:40 -0400

    pushing the stuff

--- query.py
+++ query.py
@@ -96,19 +96,40 @@ def format_nodes(transactions):
 	#build the nodes
 	nodes = []
 	for tx in transactions:
-		addresses = tx.inputs + tx.outputs
-		for data in addresses:
-			addr = data[0]
-			if addr not in positions:
-				#add index so we have it for later
-				positions[addr] = at
-				at += 1
-				#update occurances
-				occurs[addr] = 1
-			else:
-				occurs[addr] = occurs[addr] + 1
+		seen = set([])
+		for data in tx.inputs:
+			sender = data[0]
+			for data in tx.outputs:
+				receiver = data[0]
+				test = (sender, receiver)
+				if (sender, receiver) in seen:
+					continue
+				seen.add((sender, receiver))
+				if sender not in occurs:
+					occurs[sender] = 1
+				else:
+					occurs[sender] = occurs[sender] + 1
+				if receiver not in occurs:
+					occurs[receiver] = 1
+				else:
+					occurs[receiver] = occurs[receiver] + 1
+		# seen = set([])
+		# addresses = tx.inputs + tx.outputs
+		# for data in addresses:
+		# 	addr = data[0]
+		# 	if addr not in occurs:
+		# 		#update occurances
+		# 		occurs[addr] = 1
+		# 	# elif addr not in seen:
+		# 	else:
+		# 		occurs[addr] = occurs[addr] + 1
+			# seen.add(addr)
 	#build the nodes themselves
+	# print occurs
 	for addr in occurs:
+		#add index so we have it for later
+		positions[addr] = at
+		at += 1
 		#make the actual node
 		temp = {}
 		temp['address'] = addr
@@ -116,11 +137,18 @@ def format_nodes(transactions):
 		nodes.append(temp)
 	#now build the links
 	links = []
+	#for finding multiple links between 2 nodes
+	found = {}
 	for tx in transactions:
+		seen = set([])
 		for data in tx.inputs:
 			sender = data[0]
 			for data in tx.outputs:
 				receiver = data[0]
+				test = (sender, receiver)
+				if (sender, receiver) in seen:
+					continue
+				seen.add((sender, receiver))
 				#get index of sender and receiver
 				sender_ind = positions[sender]
 				receiver_ind = positions[receiver]
@@ -128,6 +156,15 @@ def format_nodes(transactions):
 				link = {}
 				link['source'] = sender_ind
 				link['target'] = receiver_ind
+				if sender_ind < receiver_ind:
+					tup = (sender_ind, receiver_ind)
+				else:
+					tup = (receiver_ind, sender_ind)
+				if tup in found:
+					found[tup] = found[tup] + 1
+				else:
+					found[tup] = 1
+				link['linkNum'] = found[tup]
 				links.append(link)
 
 	#build final graph
